a8fff53876b2eb21257f6aaaba5e360a47ab9fa8
monish <monish.narwani@focalworks.in>
2017-08-01 22:25:37 +0530

    example of modules export

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-function greet() {
-    console.log('in greet function');
-}
-greet();
-function logGreet(func) {
-    func();    
-}
-logGreet(greet);
-
-var greetMe = function () {
-    console.log('in greetMe');
-}
-greetMe();
-
-logGreet(greetMe);
-
-logGreet(function () {
-    console.log('function expression on fly');
-});
+var greet = require('./greet.js');
+greet.greet1();
