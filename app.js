b8561fe6cc615052bcc597835864e6e93a061762
monish <monish.narwani@focalworks.in>
2017-08-01 21:59:27 +0530

    lecture 17 done

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ function logGreet(func) {
     func();    
 }
 logGreet(greet);
+
+var greetMe = function () {
+    console.log('in greetMe');
+}
+greetMe();
+
+logGreet(greetMe);
+
+logGreet(function () {
+    console.log('function expression on fly');
+});
